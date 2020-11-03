357fc547da8e0d840902c406234710f265ff13b8
Yury Mazeev <yuri.mazeyev@gmail.com>
2020-11-03 17:50:50 +0100

    sdcard_sdio module stub generated using riotgen

---
+++ drivers/sdcard_sdio/sdcard_sdio.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2020 YUMA Engineering
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_sdcard_sdio
+ * @{
+ *
+ * @file
+ * @brief       Device driver implementation for the drivers_sdcard_sdio
+ *
+ * @author      Yury Mazeev <yuri.mazeyev@gmail.com>
+ *
+ * @}
+ */
+
+#include "sdcard_sdio.h"
+#include "sdcard_sdio_constants.h"
+#include "sdcard_sdio_params.h"
+
+int sdcard_sdio_init(sdcard_sdio_t *dev, const sdcard_sdio_params_t *params)
+{
+
+}
