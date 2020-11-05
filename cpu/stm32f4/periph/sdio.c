376b4d237fd920f269ebdb292fba3a31c39774c7
Yury Mazeev <yuri.mazeyev@gmail.com>
2020-11-05 19:07:57 +0100

    sdcard_sdio: read and write operations

--- cpu/stm32f4/periph/sdio.c
+++ cpu/stm32f4/periph/sdio.c
@@ -6,10 +6,6 @@
 
 #include "periph/sdio.h"
 
-enum {
-    SD_CMD_TIMEOUT = 10000,
-};
-
 // Mask for errors in card status value
 #define SD_OCR_ALL_ERRORS             ((uint32_t)0xFDFFE008U) // All possible error bits
 #define SD_OCR_OUT_OF_RANGE           ((uint32_t)0x80000000U) // The command's argument was out of allowed range
@@ -37,6 +33,10 @@ enum {
                                                 SDIO_ICR_DTIMEOUTC | SDIO_ICR_TXUNDERRC | SDIO_ICR_RXOVERRC  | \
                                                 SDIO_ICR_CMDRENDC  | SDIO_ICR_CMDSENTC  | SDIO_ICR_DATAENDC  | \
                                                 SDIO_ICR_DBCKENDC))
+
+// Bitmap to clear the SDIO command flags
+#define SDIO_ICR_CMD                  ((uint32_t)(SDIO_ICR_CCRCFAILC | SDIO_ICR_CTIMEOUTC | \
+                                                SDIO_ICR_CMDRENDC | SDIO_ICR_CMDSENTC))
 
 // Pattern for R6 response
 #define SD_CHECK_PATTERN              ((uint32_t)0x000001AAU)
@@ -71,9 +71,171 @@ void sdio_init(sdio_t bus) {
     periph_clk_en(sdio_config[bus].apbbus, sdio_config[bus].rccmask);
 }
 
-void sdio_init_card(sdio_t bus, sdcard_spi_t* card) {
-    sdio_init(bus);
-    (void)card;
+int sdio_detect_card(sdio_t bus, sdio_card_type_t* typ) {
+
+    int cmd_res = 0;
+    volatile uint32_t wait;
+    uint32_t sd_type = SD_STD_CAPACITY; // SD card capacity
+    // Enable the SDIO clock
+    dev(bus)->POWER = SDIO_POWER_PWRCTRL;
+
+    // CMD0
+    wait = SD_CMD_TIMEOUT;
+    sdio_cmd(bus, SD_CMD_GO_IDLE_STATE, 0x00, SD_RESP_NONE);
+    while (!(dev(bus)->STA & (SDIO_STA_CTIMEOUT | SDIO_STA_CMDSENT)) && --wait);
+    if ((dev(bus)->STA & SDIO_STA_CTIMEOUT) || !wait) {
+        return SDR_Timeout;
+    }
+
+    // CMD8: SEND_IF_COND. Send this command to verify SD card interface operating condition
+    // Argument: - [31:12]: Reserved (shall be set to '0')
+    //           - [11:08]: Supply Voltage (VHS) 0x1 (Range: 2.7-3.6 V)
+    //           - [07:00]: Check Pattern (recommended 0xAA)
+    sdio_cmd(bus, SD_CMD_HS_SEND_EXT_CSD, SD_CHECK_PATTERN, SD_RESP_SHORT); // CMD8
+    cmd_res = sdio_wait_R7(bus);
+    if (cmd_res == SDR_Success) {
+        // SD v2.0 or later
+
+        // Check echo-back of check pattern
+        if ((dev(bus)->RESP1 & 0x01FF) != (SD_CHECK_PATTERN & 0x01FF)) {
+            return SDR_Unsupported;
+        }
+        sd_type = SD_HIGH_CAPACITY; // SD v2.0 or later
+
+        // Issue ACMD41 command
+        wait = SD_ACMD41_TRIALS;
+        while (--wait) {
+            // Send leading command for ACMD<n> command
+            sdio_cmd(bus, SD_CMD_APP_CMD, 0, SD_RESP_SHORT); // CMD55 with RCA 0
+            cmd_res = sdio_wait_R1(bus, SD_CMD_APP_CMD);
+            if (cmd_res != SDR_Success) {
+                return cmd_res;
+            }
+
+            // ACMD41 - initiate initialization process.
+            // Set 3.0-3.3V voltage window (bit 20)
+            // Set HCS bit (30) (Host Capacity Support) to inform card what host support high capacity
+            // Set XPC bit (28) (SDXC Power Control) to use maximum performance (SDXC only)
+            sdio_cmd(bus, SD_CMD_SEND_OP_COND, SD_OCR_VOLTAGE | sd_type, SD_RESP_SHORT);
+            cmd_res = sdio_wait_R3(bus);
+            if (cmd_res != SDR_Success) {
+                return cmd_res;
+            }
+            if (dev(bus)->RESP1 & (1 << 31)) {
+                // The SD card has finished the power-up sequence
+                break;
+            }
+        }
+        if (wait == 0) {
+            // Unsupported voltage range
+            return SDR_InvalidVoltage;
+        }
+
+        // This is SDHC/SDXC card?
+        *typ = (dev(bus)->RESP1 & SD_HIGH_CAPACITY) ? SDCT_SDHC : SDCT_SDSC_V2;
+    } else if (cmd_res == SDR_Timeout) {
+        // SD v1.x or MMC
+
+        // Issue CMD55 to reset 'Illegal command' bit of the SD card
+        sdio_cmd(bus, SD_CMD_APP_CMD, 0, SD_RESP_SHORT); // CMD55 with RCA 0
+        sdio_wait_R1(bus, SD_CMD_APP_CMD);
+
+        // Issue ACMD41 command with zero argument
+        wait = SD_ACMD41_TRIALS;
+        while (--wait) {
+            // Send leading command for ACMD<n> command
+            sdio_cmd(bus, SD_CMD_APP_CMD, 0, SD_RESP_SHORT); // CMD55 with RCA 0
+            cmd_res = sdio_wait_R1(bus, SD_CMD_APP_CMD);
+            if (cmd_res != SDR_Success) {
+                return cmd_res;
+            }
+
+            // Send ACMD41 - initiate initialization process (bit HCS = 0)
+            sdio_cmd(bus, SD_CMD_SD_SEND_OP_COND, SD_OCR_VOLTAGE, SD_RESP_SHORT); // ACMD41
+            cmd_res = sdio_wait_R3(bus);
+            if (cmd_res == SDR_Timeout) {
+                // MMC will not respond to this command
+                break;
+            }
+            if (cmd_res != SDR_Success) {
+                return cmd_res;
+            }
+            if (dev(bus)->RESP1 & (1 << 31)) {
+                // The SD card has finished the power-up sequence
+                break;
+            }
+        }
+        if (wait == 0) {
+            // Unknown/Unsupported card type
+            return SDR_UnknownCard;
+        }
+        if (cmd_res != SDR_Timeout) {
+            // SD v1.x
+            *typ = SDCT_SDSC_V1; // SDv1
+        } else {
+            // MMC or not SD memory card
+
+            ////////////////////////////////////////////////////////////////
+            // This part has not been tested due to lack of MMCmicro card //
+            ////////////////////////////////////////////////////////////////
+
+            wait = SD_ACMD41_TRIALS;
+            while (--wait) {
+                // Issue CMD1: initiate initialization process.
+                sdio_cmd(bus, SD_CMD_SEND_OP_COND, SD_OCR_VOLTAGE, SD_RESP_SHORT); // CMD1
+                cmd_res = sdio_wait_R3(bus);
+                if (cmd_res != SDR_Success) {
+                    return cmd_res;
+                }
+                if (dev(bus)->RESP1 & (1 << 31)) {
+                    // The SD card has finished the power-up sequence
+                    break;
+                }
+            }
+            if (wait == 0) {
+                return SDR_UnknownCard;
+            }
+            *typ = SDCT_MMC; // MMC
+        }
+    }
+
+    return cmd_res;
+}
+
+int sdio_set_bus_width(sdio_t bus, sdio_width_t w) {
+    SDResult cmd_res = SDR_Success;
+    uint32_t clk;
+
+    // Configure new bus width
+    clk  = dev(bus)->CLKCR;
+    clk &= ~SDIO_CLKCR_WIDBUS;
+    clk |= (w & SDIO_CLKCR_WIDBUS);
+    dev(bus)->CLKCR = clk;
+
+    return cmd_res;
+}
+
+static const uint8_t stm32_sdio_clk_div[] = {118, 10, 4, 1, 0, 0};
+
+void sdio_set_bus_clock(sdio_t bus, sdio_clk_t c) {
+    uint32_t clk;
+
+    clk  = dev(bus)->CLKCR;
+    clk &= ~SDIO_CLKCR_CLKDIV;
+    clk |= (stm32_sdio_clk_div[c] & SDIO_CLKCR_CLKDIV);
+    dev(bus)->CLKCR = clk;
+    // TODO: SDIO_CLK_48MHZ - activate bypass
+}
+
+void sdio_cmd(sdio_t bus, uint8_t cmd, uint32_t arg, uint32_t resp_type) {
+    // Clear command flags
+    dev(bus)->ICR = SDIO_ICR_CMD;
+
+    // Program an argument for command
+    dev(bus)->ARG = arg;
+
+    // Program command value and response type, enable CPSM
+    dev(bus)->CMD = cmd | resp_type | SDIO_CMD_CPSMEN;
 }
 
 int sdio_wait_R1(sdio_t bus, uint8_t cmd) {
@@ -254,24 +416,226 @@ int sdio_wait_R7(sdio_t bus) {
     return SDR_NoResponse;
 }
 
-int sdio_read_blocks(sdio_t bus, int blockaddr, uint8_t* data, int blocksize, // card?
-                     int nblocks, sd_rw_response_t *state) {
-    (void)bus;
-    (void)blockaddr;
-    (void)data;
-    (void)blocksize;
-    (void)nblocks;
-    (void)state;
-    return 0;
+static int _sdio_stop_transfer(sdio_t bus) {
+    sdio_cmd(bus, SD_CMD_STOP_TRANSMISSION, 0, SD_RESP_SHORT); // CMD12
+    return sdio_wait_R1(bus, SD_CMD_STOP_TRANSMISSION);
 }
 
-int sdio_write_blocks(sdio_t bus, int blockaddr, const uint8_t *data, int blocksize,
-                      int nblocks, sd_rw_response_t *state) {
-    (void)bus;
-    (void)blockaddr;
-    (void)data;
-    (void)blocksize;
-    (void)nblocks;
-    (void)state;
-    return 0;
+int sdio_read_blocks(sdio_t bus, uint32_t addr, uint32_t *pBuf, uint32_t length) {
+    SDResult cmd_res = SDR_Success;
+    uint32_t blk_count = length >> 9; // Sectors in block
+    register uint32_t STA; // to speed up SDIO flags checking
+    register uint32_t STA_mask; // mask for SDIO flags checking
+
+    // Initialize the data control register
+    dev(bus)->DCTRL = 0;
+
+    // Clear the static SDIO flags
+    dev(bus)->ICR = SDIO_ICR_STATIC;
+
+    if (blk_count > 1) {
+        // Prepare bit checking variable for multiple block transfer
+        STA_mask = SDIO_RX_MB_FLAGS;
+        // Send READ_MULT_BLOCK command
+        sdio_cmd(bus, SD_CMD_READ_MULT_BLOCK, addr, SD_RESP_SHORT); // CMD18
+        cmd_res = sdio_wait_R1(bus, SD_CMD_READ_MULT_BLOCK);
+    } else {
+        // Prepare bit checking variable for single block transfer
+        STA_mask = SDIO_RX_SB_FLAGS;
+        // Send READ_SINGLE_BLOCK command
+        sdio_cmd(bus, SD_CMD_READ_SINGLE_BLOCK, addr, SD_RESP_SHORT); // CMD17
+        cmd_res = sdio_wait_R1(bus, SD_CMD_READ_SINGLE_BLOCK);
+    }
+    if (cmd_res != SDR_Success) {
+        return cmd_res;
+    }
+
+    // Data read timeout
+    dev(bus)->DTIMER = SD_DATA_R_TIMEOUT;
+    // Data length
+    dev(bus)->DLEN   = length;
+    // Data transfer:
+    //   transfer mode: block
+    //   direction: to card
+    //   DMA: disabled
+    //   block size: 2^9 = 512 bytes
+    //   DPSM: enabled
+    dev(bus)->DCTRL  = SDIO_DCTRL_DTDIR | (9 << 4) | SDIO_DCTRL_DTEN;
+
+    // Receive a data block from the SDIO
+    // ----> TIME CRITICAL SECTION BEGIN <----
+    do {
+        STA = dev(bus)->STA;
+        if (STA & SDIO_STA_RXFIFOHF) {
+            // The receive FIFO is half full, there are at least 8 words in it
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+            *pBuf++ = dev(bus)->FIFO;
+        }
+    } while (!(STA & STA_mask));
+    // <---- TIME CRITICAL SECTION END ---->
+
+    // Send stop transmission command in case of multiple block transfer
+    if (blk_count > 1) { // && (SDCard.Type != SDCT_MMC)
+        cmd_res = _sdio_stop_transfer(bus);
+    }
+
+    // Check for errors
+    if (STA & SDIO_XFER_ERROR_FLAGS) {
+        if (STA & SDIO_STA_DTIMEOUT) cmd_res = SDR_DataTimeout;
+        if (STA & SDIO_STA_DCRCFAIL) cmd_res = SDR_DataCRCFail;
+        if (STA & SDIO_STA_RXOVERR)  cmd_res = SDR_RXOverrun;
+    }
+
+    // Read the data remnant from RX FIFO (if there is still any data)
+    while (dev(bus)->STA & SDIO_STA_RXDAVL) {
+        *pBuf++ = dev(bus)->FIFO;
+    }
+
+    // Clear the static SDIO flags
+    dev(bus)->ICR = SDIO_ICR_STATIC;
+
+    return cmd_res;
+}
+
+int _sdio_get_card_state(sdio_t bus, uint16_t rca, uint8_t *pState) {
+    uint8_t cmd_res = SDR_Success;
+
+    // Send SEND_STATUS command
+    sdio_cmd(bus, SD_CMD_SEND_STATUS, rca << 16, SD_RESP_SHORT); // CMD13
+    cmd_res = sdio_wait_R1(bus, SD_CMD_SEND_STATUS);
+    if (cmd_res != SDR_Success) {
+        *pState = SD_STATE_ERROR;
+        return cmd_res;
+    }
+
+    // Find out a card status
+    *pState = (dev(bus)->RESP1 & 0x1e00) >> 9;
+
+    // Check for errors
+    return cmd_res;
+}
+
+int sdio_write_blocks(sdio_t bus, uint16_t rca, uint32_t addr, uint32_t *pBuf, uint32_t length) {
+    SDResult cmd_res = SDR_Success;
+    uint32_t blk_count = length >> 9; // Sectors in block
+    uint32_t STA; // To speed up SDIO flags checking
+    register uint32_t STA_mask; // Mask for SDIO flags checking
+    uint32_t data_sent = 0; // Counter of transferred bytes
+    uint8_t card_state; // Card state
+
+    // Initialize the data control register
+    dev(bus)->DCTRL = 0;
+
+    if (blk_count > 1) {
+        // Prepare bit checking variable for multiple block transfer
+        STA_mask = SDIO_TX_MB_FLAGS;
+        // Send WRITE_MULTIPLE_BLOCK command
+        sdio_cmd(bus, SD_CMD_WRITE_MULTIPLE_BLOCK, addr, SD_RESP_SHORT); // CMD25
+        cmd_res = sdio_wait_R1(bus, SD_CMD_WRITE_MULTIPLE_BLOCK);
+    } else {
+        // Prepare bit checking variable for single block transfer
+        STA_mask = SDIO_TX_SB_FLAGS;
+        // Send WRITE_BLOCK command
+        sdio_cmd(bus, SD_CMD_WRITE_BLOCK, addr, SD_RESP_SHORT); // CMD24
+        cmd_res = sdio_wait_R1(bus, SD_CMD_WRITE_BLOCK);
+    }
+    if (cmd_res != SDR_Success) {
+        return cmd_res;
+    }
+
+    // Clear the static SDIO flags
+    dev(bus)->ICR = SDIO_ICR_STATIC;
+
+    // Data write timeout
+    dev(bus)->DTIMER = SD_DATA_W_TIMEOUT;
+    // Data length
+    dev(bus)->DLEN = length;
+    // Data transfer:
+    //   transfer mode: block
+    //   direction: to card
+    //   DMA: disabled
+    //   block size: 2^9 = 512 bytes
+    //   DPSM: enabled
+    dev(bus)->DCTRL = (9 << 4) | SDIO_DCTRL_DTEN;
+
+    // Transfer data block to the SDIO
+    // ----> TIME CRITICAL SECTION BEGIN <----
+    if (!(length & 0x1F)) {
+        // The block length is multiple of 32, simplified transfer procedure can be used
+        do {
+            if ((dev(bus)->STA & SDIO_STA_TXFIFOHE) && (data_sent < length)) {
+                // The TX FIFO is half empty, at least 8 words can be written
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                dev(bus)->FIFO = *pBuf++;
+                data_sent += 32;
+            }
+        } while (!(dev(bus)->STA & STA_mask));
+    } else {
+        // Since the block length is not a multiple of 32, it is necessary to apply additional calculations
+        do {
+            if ((dev(bus)->STA & SDIO_STA_TXFIFOHE) && (data_sent < length)) {
+                // TX FIFO half empty, at least 8 words can be written
+                uint32_t data_left = length - data_sent;
+                if (data_left < 32) {
+                    // Write last portion of data to the TX FIFO
+                    data_left = ((data_left & 0x03) == 0) ? (data_left >> 2) : ((data_left >> 2) + 1);
+                    data_sent += data_left << 2;
+                    while (data_left--) {
+                        dev(bus)->FIFO = *pBuf++;
+                    }
+                } else {
+                    // Write 8 words to the TX FIFO
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    dev(bus)->FIFO = *pBuf++;
+                    data_sent += 32;
+                }
+            }
+        } while (!(dev(bus)->STA & STA_mask));
+    }
+    // <---- TIME CRITICAL SECTION END ---->
+
+    // Save STA register value for further analysis
+    STA = dev(bus)->STA;
+
+    // Send stop transmission command in case of multiple block transfer
+    if (blk_count > 1) { // && (SDCard.Type != SDCT_MMC)
+        cmd_res = _sdio_stop_transfer(bus);
+    }
+
+    // Check for errors
+    if (STA & SDIO_XFER_ERROR_FLAGS) {
+        if (STA & SDIO_STA_DTIMEOUT) cmd_res = SDR_DataTimeout;
+        if (STA & SDIO_STA_DCRCFAIL) cmd_res = SDR_DataCRCFail;
+        if (STA & SDIO_STA_TXUNDERR) cmd_res = SDR_TXUnderrun;
+    }
+
+    // Wait while the card is in programming state
+    do {
+        if (_sdio_get_card_state(bus, rca, &card_state) != SDR_Success) {
+            break;
+        }
+    } while ((card_state == SD_STATE_PRG) || (card_state == SD_STATE_RCV));
+
+    // Clear the static SDIO flags
+    dev(bus)->ICR = SDIO_ICR_STATIC;
+
+    return cmd_res;
 }
--- drivers/include/periph/sdio.h
+++ drivers/include/periph/sdio.h
@@ -14,6 +14,10 @@
 typedef unsigned int sdio_t;
 #endif
 
+#ifndef SDIO_DEV
+#define SDIO_DEV(x)      (x)
+#endif
+
 #ifndef HAVE_SDIO_CLK_T
 typedef enum {
     SDIO_CLK_400KHZ = 0,
@@ -30,6 +34,15 @@ typedef enum {
     SDIO_4bit,
     SDIO_8Bit,
 } sdio_width_t;
+
+// Card type
+typedef enum {
+    SDCT_UNKNOWN = 0x00,
+    SDCT_SDSC_V1 = 0x01,  // Standard capacity SD card v1.0
+    SDCT_SDSC_V2 = 0x02,  // Standard capacity SD card v2.0
+    SDCT_MMC     = 0x03,  // MMC
+    SDCT_SDHC    = 0x04   // High capacity SD card (SDHC or SDXC)
+} sdio_card_type_t;
 
 typedef enum {
     SDR_Success             = 0x00,
@@ -72,13 +85,154 @@ typedef enum {
     SDR_UnknownError        = 0xFF   // Unknown error
 } SDResult;
 
+// SD commands  index
+#define SD_CMD_GO_IDLE_STATE          ((uint8_t)0U)
+#define SD_CMD_SEND_OP_COND           ((uint8_t)1U)  // MMC only
+#define SD_CMD_ALL_SEND_CID           ((uint8_t)2U)  // Not supported in SPI mode
+#define SD_CMD_SEND_REL_ADDR          ((uint8_t)3U)  // Not supported in SPI mode
+#define SD_CMD_SWITCH_FUNC            ((uint8_t)6U)
+#define SD_CMD_SEL_DESEL_CARD         ((uint8_t)7U)  // Not supported in SPI mode
+#define SD_CMD_HS_SEND_EXT_CSD        ((uint8_t)8U)
+#define SD_CMD_SEND_CSD               ((uint8_t)9U)
+#define SD_CMD_SEND_CID               ((uint8_t)10U)
+#define SD_CMD_READ_DAT_UNTIL_STOP    ((uint8_t)11U) // Not supported in SPI mode
+#define SD_CMD_STOP_TRANSMISSION      ((uint8_t)12U)
+#define SD_CMD_SEND_STATUS            ((uint8_t)13U)
+#define SD_CMD_GO_INACTIVE_STATE      ((uint8_t)15U) // Not supported in SPI mode
+#define SD_CMD_SET_BLOCKLEN           ((uint8_t)16U)
+#define SD_CMD_READ_SINGLE_BLOCK      ((uint8_t)17U)
+#define SD_CMD_READ_MULT_BLOCK        ((uint8_t)18U)
+#define SD_CMD_WRITE_DAT_UNTIL_STOP   ((uint8_t)20U) // Not supported in SPI mode
+#define SD_CMD_WRITE_BLOCK            ((uint8_t)24U)
+#define SD_CMD_WRITE_MULTIPLE_BLOCK   ((uint8_t)25U)
+#define SD_CMD_PROG_CSD               ((uint8_t)27U)
+#define SD_CMD_SET_WRITE_PROT         ((uint8_t)28U) // Not supported in SPI mode
+#define SD_CMD_CLR_WRITE_PROT         ((uint8_t)29U) // Not supported in SPI mode
+#define SD_CMD_SEND_WRITE_PROT        ((uint8_t)30U) // Not supported in SPI mode
+#define SD_CMD_ERASE                  ((uint8_t)38U)
+#define SD_CMD_LOCK_UNLOCK            ((uint8_t)42U)
+#define SD_CMD_APP_CMD                ((uint8_t)55U)
+#define SD_CMD_READ_OCR               ((uint8_t)58U) // Read OCR register
+#define SD_CMD_CRC_ON_OFF             ((uint8_t)59U) // On/Off CRC check by SD Card (in SPI mode)
+
+// Trials count for ACMD41
+#define SD_ACMD41_TRIALS              ((uint32_t)0x0000FFFFU)
+
+// SDIO clock divider
+#define SD_CLK_DIV_400K               ((uint32_t)0x00000076U) // SDIO clock 400kHz  (48MHz / (0x76 + 2) = 400kHz)
+#define SD_CLK_DIV_1M                 ((uint32_t)0x0000002EU) // SDIO clock 1MHz    (48MHz / (0x2e + 2) = 1MHz)
+#define SD_CLK_DIV_2M                 ((uint32_t)0x00000016U) // SDIO clock 2MHz    (48MHz / (0x16 + 2) = 2MHz)
+#define SD_CLK_DIV_4M                 ((uint32_t)0x0000000AU) // SDIO clock 4MHz    (48MHz / (0x0a + 2) = 4MHz)
+#define SD_CLK_DIV_6M85               ((uint32_t)0x00000005U) // SDIO clock 6.85MHz (48MHz / (0x05 + 2) = 6.85MHz)
+#define SD_CLK_DIV_8M                 ((uint32_t)0x00000004U) // SDIO clock 8MHz    (48MHz / (0x04 + 2) = 8MHz)
+#define SD_CLK_DIV_9M6                ((uint32_t)0x00000003U) // SDIO clock 9.6MHz  (48MHz / (0x03 + 2) = 9.6MHz)
+#define SD_CLK_DIV_12M                ((uint32_t)0x00000002U) // SDIO clock 12MHz   (48MHz / (0x02 + 2) = 12MHz)
+#define SD_CLK_DIV_16M                ((uint32_t)0x00000001U) // SDIO clock 16MHz   (48MHz / (0x01 + 2) = 16MHz)
+#define SD_CLK_DIV_24M                ((uint32_t)0x00000000U) // SDIO clock 24MHz   (48MHz / (0x00 + 2) = 24MHz)
+
+// SDIO clocks for initialization and data transfer
+#define SD_CLK_DIV_INIT               (SD_CLK_DIV_400K) // SDIO initialization frequency (400kHz)
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_400K) // SDIO data transfer 400kHz
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_1M)   // SDIO data transfer 1MHz
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_8M)   // SDIO data transfer 8MHz
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_9M6)  // SDIO data transfer 9.6MHz
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_12M)  // SDIO data transfer 12MHz
+//#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_16M)  // SDIO data transfer 16MHz
+#define SD_CLK_DIV_TRAN               (SD_CLK_DIV_24M) // SDIO data transfer frequency
+
+// SDIO CMD response type
+#define SD_RESP_NONE                  ((uint32_t)0x00000000U) // No response
+#define SD_RESP_SHORT                 (SDIO_CMD_WAITRESP_0)  // Short response
+#define SD_RESP_LONG                  (SDIO_CMD_WAITRESP_1)    // Long response
+
+// Following commands are SD Card Specific commands.
+// SD_CMD_APP_CMD should be sent before sending these commands.
+#define SD_CMD_SET_BUS_WIDTH          ((uint8_t)6U)  // ACMD6
+#define SD_CMD_SD_SEND_OP_COND        ((uint8_t)41U) // ACMD41
+#define SD_CMD_SET_CLR_CARD_DETECT    ((uint8_t)42U) // ACMD42
+#define SD_CMD_SEND_SCR               ((uint8_t)51U) // ACMD51
+
+// Pattern for R6 response
+#define SD_CHECK_PATTERN              ((uint32_t)0x000001AAU)
+
+// R6 response error bits
+#define SD_R6_GENERAL_UNKNOWN_ERROR   ((uint32_t)0x00002000U)
+#define SD_R6_ILLEGAL_CMD             ((uint32_t)0x00004000U)
+#define SD_R6_COM_CRC_FAILED          ((uint32_t)0x00008000U)
+
+// Argument for ACMD41 to select voltage window
+#define SD_OCR_VOLTAGE                ((uint32_t)0x80100000U)
+
+// Mask for errors in card status value
+#define SD_OCR_ALL_ERRORS             ((uint32_t)0xFDFFE008U) // All possible error bits
+#define SD_OCR_OUT_OF_RANGE           ((uint32_t)0x80000000U) // The command's argument was out of allowed range
+#define SD_OCR_ADDRESS_ERROR          ((uint32_t)0x40000000U) // A misaligned address used in the command
+#define SD_OCR_BLOCK_LEN_ERROR        ((uint32_t)0x20000000U) // The transfer block length is not allowed for this card
+#define SD_OCR_ERASE_SEQ_ERROR        ((uint32_t)0x10000000U) // An error in the sequence of erase commands occurred
+#define SD_OCR_ERASE_PARAM            ((uint32_t)0x08000000U) // An invalid selection of write-blocks for erase occurred
+#define SD_OCR_WP_VIOLATION           ((uint32_t)0x04000000U) // Attempt to write to a protected block or to the write protected card
+#define SD_OCR_LOCK_UNLOCK_FAILED     ((uint32_t)0x01000000U) // Sequence or password error in lock/unlock card command
+#define SD_OCR_COM_CRC_ERROR          ((uint32_t)0x00800000U) // The CRC check of the previous command failed
+#define SD_OCR_ILLEGAL_COMMAND        ((uint32_t)0x00400000U) // Command not legal for the card state
+#define SD_OCR_CARD_ECC_FAILED        ((uint32_t)0x00200000U) // Card internal ECC was applied but failed to correct the data
+#define SD_OCR_CC_ERROR               ((uint32_t)0x00100000U) // Internal card controller error
+#define SD_OCR_ERROR                  ((uint32_t)0x00080000U) // A general or an unknown error occurred during the operation
+#define SD_OCR_STREAM_R_UNDERRUN      ((uint32_t)0x00040000U) // The card could not sustain data transfer in stream read operation
+#define SD_OCR_STREAM_W_OVERRUN       ((uint32_t)0x00020000U) // The card could not sustain data programming in stream mode
+#define SD_OCR_CSD_OVERWRITE          ((uint32_t)0x00010000U) // CSD overwrite error
+#define SD_OCR_WP_ERASE_SKIP          ((uint32_t)0x00008000U) // Only partial address space was erased
+#define SD_OCR_CARD_ECC_DISABLED      ((uint32_t)0x00004000U) // The command has been executed without using the internal ECC
+#define SD_OCR_ERASE_RESET            ((uint32_t)0x00002000U) // An erase sequence was cleared before executing
+#define SD_OCR_AKE_SEQ_ERROR          ((uint32_t)0x00000008U) // Error in the sequence of the authentication process
+
+// Card state (OCR[12:9] bits CURRENT_STATE)
+#define SD_STATE_IDLE                 ((uint8_t)0x00U) // Idle
+#define SD_STATE_READY                ((uint8_t)0x01U) // Ready
+#define SD_STATE_IDENT                ((uint8_t)0x02U) // Identification
+#define SD_STATE_STBY                 ((uint8_t)0x03U) // Stand-by
+#define SD_STATE_TRAN                 ((uint8_t)0x04U) // Transfer
+#define SD_STATE_DATA                 ((uint8_t)0x05U) // Sending data
+#define SD_STATE_RCV                  ((uint8_t)0x06U) // Receive data
+#define SD_STATE_PRG                  ((uint8_t)0x07U) // Programming
+#define SD_STATE_DIS                  ((uint8_t)0x08U) // Disconnect
+#define SD_STATE_ERROR                ((uint8_t)0xFFU) // Error or unknown state
+
+// Mask for ACMD41
+#define SD_STD_CAPACITY               ((uint32_t)0x00000000U)
+#define SD_HIGH_CAPACITY              ((uint32_t)0x40000000U)
+
+// Timeout for CMD0 or CMD8
+#define SD_CMD_TIMEOUT                ((uint32_t)0x00010000U)
+
+// SDIO timeout for data transfer ((48MHz / CLKDIV / 1000) * timeout_ms)
+#define SD_DATA_R_TIMEOUT             ((uint32_t)((48000000U / (SD_CLK_DIV_TRAN + 2U) / 1000U) * 100U)) // Data read timeout is 100ms
+#define SD_DATA_W_TIMEOUT             ((uint32_t)((48000000U / (SD_CLK_DIV_TRAN + 2U) / 1000U) * 250U)) // Date write timeout is 250ms
+
+// SDIO transfer flags
+#define SDIO_XFER_COMMON_FLAGS        (SDIO_STA_DTIMEOUT | SDIO_STA_DCRCFAIL)
+
+// SDIO flags for single block receive
+#define SDIO_RX_SB_FLAGS              (SDIO_XFER_COMMON_FLAGS | SDIO_STA_DBCKEND | SDIO_STA_RXOVERR)
+
+// SDIO flags for multiple block receive
+#define SDIO_RX_MB_FLAGS              (SDIO_XFER_COMMON_FLAGS | SDIO_STA_DATAEND | SDIO_STA_RXOVERR)
+
+// SDIO flags for single block transmit
+#define SDIO_TX_SB_FLAGS              (SDIO_XFER_COMMON_FLAGS | SDIO_STA_DBCKEND | SDIO_STA_TXUNDERR)
+
+// SDIO flags for multiple block transmit
+#define SDIO_TX_MB_FLAGS              (SDIO_XFER_COMMON_FLAGS | SDIO_STA_DATAEND | SDIO_STA_TXUNDERR)
+
+// SDIO transfer error flags
+#define SDIO_XFER_ERROR_FLAGS         (SDIO_XFER_COMMON_FLAGS | SDIO_STA_TXUNDERR | SDIO_STA_RXOVERR)
+
 // stm32's SDIO peripheral related functions
 
 void sdio_init(sdio_t bus);
 
-void sdio_init_card(sdio_t bus, sdcard_spi_t* card);
+int sdio_detect_card(sdio_t bus, sdio_card_type_t* typ);
 
-void sdio_set_bus_width(sdio_t bus, sdio_width_t w);
+int sdio_set_bus_width(sdio_t bus, sdio_width_t w);
 
 void sdio_set_bus_clock(sdio_t bus, sdio_clk_t clk);
 
@@ -97,10 +251,8 @@ int sdio_wait_R6(sdio_t bus, uint8_t cmd, uint16_t* rca);
 
 int sdio_wait_R7(sdio_t bus);
 
-int sdio_read_blocks(sdio_t bus, int blockaddr, uint8_t* data, int blocksize,
-                     int nblocks, sd_rw_response_t *state);
+int sdio_read_blocks(sdio_t bus, uint32_t addr, uint32_t *pBuf, uint32_t length);
 
-int sdio_write_blocks(sdio_t bus, int blockaddr, const uint8_t *data, int blocksize,
-                     int nblocks, sd_rw_response_t *state);
+int sdio_write_blocks(sdio_t bus, uint16_t rca, uint32_t addr, uint32_t *pBuf, uint32_t length);
 
 #endif // PERIPH_SDIO_H
