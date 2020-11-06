d176dd9cb1eaa07867be83d02f9036e7f5098cad
Yury Mazeev <yuri.mazeyev@gmail.com>
2020-11-06 13:06:01 +0100

    generated blank mtd_sdcard_sdio driver

---
+++ drivers/mtd_sdcard_sdio/include/mtd_sdcard_sdio_constants.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 YUMA Engineering
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_mtd_sdcard_sdio
+ * @{
+ *
+ * @file
+ * @brief       Internal addresses, registers and constants
+ *
+ * @author      Yury Mazeev <yuri.mazeyev@gmail.com>
+ */
+
+#ifndef MTD_SDCARD_SDIO_CONSTANTS_H
+#define MTD_SDCARD_SDIO_CONSTANTS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* define here the addresses, register and constants of the driver */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MTD_SDCARD_SDIO_CONSTANTS_H */
+/** @} */
